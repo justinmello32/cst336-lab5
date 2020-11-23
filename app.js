431f8c8bb00e049ff234cdf3b5c7c9748ec25393
Justin Mello <mello23@llnl.gov>
2020-11-22 21:00:17 -0800

    Finishing part 4.

--- app.js
+++ app.js
@@ -12,6 +12,20 @@ app.get("/", async function(req, res){
 	res.render("index", {"imageUrl": data.urls.small});
 });
 
+app.get("/search", async function(req, res){
+
+	let keyword = "";
+	if (req.query.keyword) {
+		keyword = req.query.keyword;
+	}
+
+	let apiUrl = `https://api.unsplash.com/photos/random/?client_id=mXmDF87g_R0kGl7TH_rwOrLsnuIAVDBZTX0OHInloFs&featured=true&orientation=landscape&query=${keyword}`;
+	let response = await fetch(apiUrl);
+	let data = await response.json();
+	res.render("results", {"imageUrl": data.urls.small});
+
+});
+
 //Starting Server
 app.listen(process.env.PORT, process.env.IP, function(){
 	console.log("Express server is running");
