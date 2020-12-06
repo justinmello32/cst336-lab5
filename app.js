ff70162c75f0f8bd96fa9b71f26474475831b385
Justin Mello <justinmello32@hotmail.com>
2020-12-06 04:23:26 +0000

    Finished with part 5.

--- app.js
+++ app.js
@@ -19,10 +19,16 @@ app.get("/search", async function(req, res){
 		keyword = req.query.keyword;
 	}
 
-	let apiUrl = `https://api.unsplash.com/photos/random/?client_id=mXmDF87g_R0kGl7TH_rwOrLsnuIAVDBZTX0OHInloFs&featured=true&orientation=landscape&query=${keyword}`;
+	let apiUrl = `https://api.unsplash.com/photos/random/?count=9&client_id=mXmDF87g_R0kGl7TH_rwOrLsnuIAVDBZTX0OHInloFs&featured=true&orientation=landscape&query=${keyword}`;
 	let response = await fetch(apiUrl);
 	let data = await response.json();
-	res.render("results", {"imageUrl": data.urls.small});
+	
+	let imageUrlArray = [];
+	for(let i=0; i < data.length; i++) {
+		imageUrlArray.push(data[i].urls.small);
+	}
+	
+	res.render("results", {"imageUrl": data[0].urls.small, "imageUrlArray":imageUrlArray});
 
 });
 
